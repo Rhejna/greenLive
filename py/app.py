d93d36d3fdb482e15a127e4ed058024363812047
NYEMB NDJEM EONE ANDRE KEVIN <86581549+AndreKev@users.noreply.github.com>
2023-10-13 16:12:28 +0100

    Update app.py
    Corrected syntax error

--- py/app.py
+++ py/app.py
@@ -169,7 +169,7 @@ class SetOpenAPI(Resource):
         try:
             data = request.get_json()
             if not data:
-                return {"error": "invalid format."), 400
+                return {"error": "invalid format."}, 400
             if data["pass"] = "greenlive":
                 prediction.setopenapi(data["key"])
                 return "Updated API key"
